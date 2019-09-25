b4958c8cbc9a93feebecb1ab4efeb725e61d6a0d
Fogelman <ddfogelman@gmail.com>
2019-09-25 12:17:27 -0300

    UPDATE - tests

--- tests/main.py
+++ tests/main.py
@@ -2,15 +2,25 @@ import subprocess
 import os
 import unittest
 
-class TestCase(unittest.TestCase):
-    def test_first(self):
-        pass
+PASSWORD='megadados'
 
-@classmethod
-def setup(testCase):
-    with open(os.path.join('..', 'birdbook.sql')) as f:
-        res = subprocess.run(f'mysql -u root -p{PASSWORD}'.split(), stdin=f)
-        print(res) 
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        with open(os.path.join(os.path.dirname(__file__), '..','birdbook.sql')) as f:
+            res = subprocess.run(f'mysql -u root -p{PASSWORD} -h 0.0.0.0'.split(), stdin=f)
+            print(res) 
+    
+    
+    def test_string(self):
+        a = 'some'
+        b = 'some'
+        self.assertEqual(a, b)
+
+    def tearDown(self):
+        print('Terminando...')
+
 
 if __name__ == '__main__':
     unittest.main()
