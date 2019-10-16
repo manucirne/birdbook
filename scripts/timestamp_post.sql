16543a31cb2d9213ba93d237d4a48a0c51cdb462
manucirne <manucirne@gmail.com>
2019-10-16 16:55:56 -0300

    correção no time stamp do post e duas procedures

--- scripts/timestamp_post.sql
+++ scripts/timestamp_post.sql
@@ -1,3 +1,3 @@
 ALTER TABLE POST
 ADD stamp_post TIMESTAMP DEFAULT current_timestamp NOT NULL COMMENT 'momento em que a publicação do post aconteceu';
-CREATE INDEX stamp_post ON POST (stamp_post ASC);
+CREATE INDEX stamp_post ON POST (stamp_post DESC);
